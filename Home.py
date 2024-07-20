2fa71e7126a4638ec17c2567fa98143fe3b818d1
김은지 <83392404+still3901@users.noreply.github.com>
2024-07-20 09:57:07 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.set_page_config(page_icon="💸", layout="wide")
+# st.set_page_config(page_icon="💸", layout="wide")
 import pandas as pd
 import numpy as np
 import os
@@ -15,6 +15,7 @@ import base64
 from fuzzywuzzy import process
 
 
+st.set_page_config(page_icon="💸", layout="wide")
 
 # 초기 세션 상태 설정
 def initialize_session_state():
@@ -50,7 +51,6 @@ initialize_session_state()
 # 설문조사 부분
 st.title(":green[미래를 위한 가장 효과적인 투자] :sunglasses:")
 st.subheader(":green[<ESG마스터> 는 ESG 데이터를 기반으로 최적의 금융 투자를 돕는 플랫폼 입니다.]")
-# st.markdown("<hr>", unsafe_allow_html=True)  # 구분선 추가
 
 # 로컬 이미지를 base64로 인코딩하는 함수
 def get_base64_of_bin_file(bin_file):
@@ -65,8 +65,6 @@ background_image_style = f"""
 .stApp {{
     background: url("data:image/jpg;base64,{img_base64}") no-repeat center center fixed;
     background-size: cover;
-    
-
 }}
 </style>
 """
@@ -118,6 +116,76 @@ st.markdown("""
     </style>
     """, unsafe_allow_html=True)
 
+def esg_text():
+    st.markdown('<h2 class="white-text">ESG 등급의 중요성에 대해 알아보세요.</h2>', unsafe_allow_html=True)
+
+    if 'active_tab' not in st.session_state:
+        st.session_state['active_tab'] = None
+
+    def switch_tab(tab_index):
+        st.session_state['active_tab'] = tab_index
+
+    st.markdown("""
+        <style>
+        .white-text {
+            color: white;
+        }
+        .stButton button {
+            margin: 0.5em;
+        }
+        </style>
+    """, unsafe_allow_html=True)
+
+    tabs = ["ESG 등급의 중요성", "종합 등급 체계", "투자 유치와의 관계", "중소기업 및 스타트업의 혜택", "기업 이미지와 소비자 행동"]
+    
+    # 상단에 버튼을 배치합니다.
+    col1, col2, col3, col4, col5 = st.columns(5)
+    if col1.button(tabs[0], key="tab0"):
+        switch_tab(0)
+    if col2.button(tabs[1], key="tab1"):
+        switch_tab(1)
+    if col3.button(tabs[2], key="tab2"):
+        switch_tab(2)
+    if col4.button(tabs[3], key="tab3"):
+        switch_tab(3)
+    if col5.button(tabs[4], key="tab4"):
+        switch_tab(4)
+
+    active_tab = st.session_state['active_tab']
+    
+    if active_tab is not None:
+        if active_tab == 0:
+            st.markdown('<h2 class="white-text">ESG 등급의 중요성</h2>', unsafe_allow_html=True)
+            st.markdown('<h3 class="white-text">ESG 등급은 환경(Environmental), 사회(Social), 지배구조(Governance) 측면에서 기업의 성과를 평가하여 부여하는 등급으로, 기업의 지속 가능성을 판단하는 중요한 지표입니다.</h3>', unsafe_allow_html=True)
+        
+        elif active_tab == 1:
+            st.markdown('<h2 class="white-text">종합 등급 체계</h2>', unsafe_allow_html=True)
+            st.markdown('''
+                <h3 class="white-text">
+                종합 등급은 다음과 같이 구성됩니다<br><br>
+                A+ (최우수)<br>
+                A (우수)<br>
+                B+ (양호)<br>
+                B (보통)<br>
+                C (미흡)<br>
+                D (매우 미흡)
+                </h3>
+                ''', unsafe_allow_html=True)
+        
+        elif active_tab == 2:
+            st.markdown('<h2 class="white-text">투자 유치와의 관계</h2>', unsafe_allow_html=True)
+            st.markdown('<h3 class="white-text">종합 등급에서 B+ 이상의 등급을 받은 기업은 ESG 경영 실행 기업으로 인정되어 국민연금의 ESG 투자규모 384.1조원(직접운용 99.7조원, 위탁운용 284.4조원)에 해당하는 투자를 받을 수 있는 기본 요건을 충족하게 됩니다.<br><br> 등급이 높을수록 벤치마크 대비(최대 10배) 많은 투자를 받을 수 있는 기회가 확대됩니다. 반면, D등급을 받은 기업은 벤치마크를 초과해 편입하지 않도록 금지되어 있습니다.<br><br> 글로벌 대기업의 경우, 블랙록과 같은 외국 투자사들이 ESG 경영이 미흡한(C 이하) 기업에서 투자를 회수하고, ESG 경영이 우수한(A 이상) 기업에 투자하는 경향이 강해지고 있습니다.</h3>', unsafe_allow_html=True)
+
+        elif active_tab == 3:
+            st.markdown('<h2 class="white-text">중소기업 및 스타트업의 혜택</h2>', unsafe_allow_html=True)
+            st.markdown('<h3 class="white-text">중소기업 및 스타트업에게도 ESG 등급 평가는 매우 중요합니다.<br><br> ESG 경영 확인서를 받은 기업은 KB국민은행에서 기준금리+가산금리(4.5%)로 최대 10억원까지 대출을 받을 수 있으며, 높은 등급을 받을수록 최대 1.4%의 추가 우대 금리를 적용받을 수 있습니다.<br><br> IBK 기업은행, 하나은행 등에서도 우대 대출을 받을 수 있으며, 대기업의 협력업체는 추가적인 우대금리 대출을 받을 수 있습니다. <br><br>대표적으로 SK그룹 협력업체의 경우 SK그룹과 국민은행이 공동 출자한 1.2조원 규모 내에서 무이자로 대출을 받을 수 있습니다.</h3>', unsafe_allow_html=True)
+
+        elif active_tab == 4:
+            st.markdown('<h2 class="white-text">기업 이미지와 소비자 행동</h2>', unsafe_allow_html=True)
+            st.markdown('<h3 class="white-text">소비자들은 단순한 정량적 가치보다 정성적 가치, 즉 ESG를 더 중요하게 판단하여 소비하는 경향이 증가하고 있습니다. <br><br>ESG 등급이 높은 기업은 소비자들로부터 긍정적인 이미지와 신뢰를 얻을 수 있으며, 이는 매출 증가와 브랜드 가치를 높이는 데 기여합니다.</h3>', unsafe_allow_html=True)
+
+
+
 # 메인 함수: 레이아웃 및 사용자 상호작용 처리
 def main():
 
@@ -148,8 +216,7 @@ def main():
 
     st.markdown(vertical_line_style, unsafe_allow_html=True)
 
-        
-        
+    
     # 기업 상세 페이지 섹션
     with col3:
         st.markdown("<h2 style='margin-bottom: -35px; color: #FFFFFF; text-shadow: 0 0 5px #000000;'>기업 상세 페이지</h2>", unsafe_allow_html=True)
@@ -157,6 +224,8 @@ def main():
         # 기업 선택 자동완성 입력 창
         selected_company = st.text_input("", placeholder="종목명을 입력하세요", key="company_input")
         df = st.session_state.df_0702
+        df = df[df['years'] == 2023]
+        
         if selected_company:
             company_names = df['회사명'].str.lower().unique()  # 기업명을 소문자로 변환하여 비교
             matches = process.extract(selected_company.lower(), company_names, limit=11)
@@ -164,16 +233,18 @@ def main():
 
             if len(filtered_matches) == 1:
                 selected_company = filtered_matches[0]
+            elif len(filtered_matches) > 1:
+                selected_company = st.selectbox("", filtered_matches)
+            else:
+                selected_company = None
 
-            selected_company = st.selectbox("", filtered_matches)
-        
         if st.button("바로가기", key="detail") and selected_company:
             # 회사 이름을 세션 상태에 저장
             st.session_state.selected_company = selected_company.upper()
             switch_page('기업 상세 정보 페이지')
 
-
-
+    esg_text()
+    
 if __name__ == "__main__":
     main()
 
